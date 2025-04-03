f6a99775f289cb7bb3d3fed30cd093140d949e25
Fred Fettinger <fettinge@gmail.com>
2025-04-03 18:52:17 -0500

    show description of roll results

--- main.py
+++ main.py
@@ -36,17 +36,55 @@ def define_env(env: MacrosPlugin):
     def stress(amount):
         return f'`{amount}`:fontawesome-solid-heart-crack:'
 
-    @env.macro
-    def d6_roll(number):
+    def d6_roll_internal(number):
         if 1 <= number <= 6:
             return f'`{number}`:material-dice-{number}:'
         else:
             return f'`{number}`:game_die:'
 
+    def d6_roll_outcome(number):
+        if number == 6:
+            return 'full'
+        if number in (4, 5):
+            return 'partial'
+        if number in (1, 2, 3):
+            return 'bad'
+
+    def d6_rolls_outcome(numbers, result):
+        if numbers.count(6) >= 2:
+            return 'critical'
+        else:
+            return d6_roll_outcome(result)
+
+    def outcome_text(outcome):
+        if outcome == 'critical':
+            return 'Critical Success'
+        elif outcome == 'full':
+            return 'Full Success'
+        elif outcome == 'partial':
+            return 'Partial Success'
+        elif outcome == 'bad':
+            return 'Bad Outcome'
+
+    OUTCOME_ICON = ':fontawesome-solid-font-awesome:'
+
+    def outcome_label(outcome):
+        text = outcome_text(outcome)
+        return f'{OUTCOME_ICON}{{ .outcome-{outcome} }} {text}'
+
     @env.macro
-    def d6_rolls(numbers, lowest=False):
+    def d6_roll(number):
+        roll = d6_roll_internal(number)
+        outcome = outcome_label(d6_roll_outcome(number))
+        return f'{roll} {outcome}'
+
+    @env.macro
+    def d6_rolls(numbers, lowest=False, outcome=True):
         op, extra = (min, '(lowest) ') if lowest else (max, '')
-        return '[' + ', '.join([d6_roll(n) for n in numbers]) + '] `=` ' + extra + d6_roll(op(numbers))
+        result = op(numbers)
+        rolls = [d6_roll_internal(n) for n in numbers]
+        outcome = outcome_label(d6_rolls_outcome(numbers, result))
+        return f'[{", ".join(rolls)}] `=` {extra}{d6_roll_internal(result)} {outcome}'
 
     def character_link(character):
         return f'[{character["name"]}]({character["url"]})'
